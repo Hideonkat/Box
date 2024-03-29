29b7e783c2aae4b2e028cb08cba87a3a3dae543f
Hideonkat <danghung0711dh@gmail.com>
2024-03-30 02:47:53 +0700

    gud

--- Assets/Game/Scripts/Controller.cs
+++ Assets/Game/Scripts/Controller.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
+    
     [SerializeField] private float moveSpeed = 5.0f;
     [SerializeField] private float jumpForce = 5.0f;
     [SerializeField] Transform groundCheck;
+    [SerializeField] Transform WallCheck;
+    [SerializeField] LayerMask wallLayer;
+
+    private float horizontal;
+    private bool isWallSlide;
+    private float wallSlideSpeed = 1.0f;
+
+
     public LayerMask groundLayer;
     private Rigidbody2D rb;
     private bool isGround;
@@ -20,6 +29,44 @@ public class Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        isGround = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if(horizontal != 0) 
+        {
+            Move(horizontal);
+        }
+        if(isGround && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Jump();
+        }
+        WallSlide();
+       
+
+    }
+    private bool isWall()
+    {
+        return Physics2D.OverlapCircle(WallCheck.position, 0.2f, wallLayer);
+    }
+
+    private void WallSlide()
+    {
+        if (isWall() && !isGround && horizontal != 0 )
+        {
+            isWallSlide = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlideSpeed, float.MaxValue));
+        }
+        else
+        {
+            isWallSlide = false;
+        }
+    }
+    private void Move(float horizontal)
+    {
+        rb.velocity = new Vector2 (horizontal * moveSpeed,rb.velocity.y);
+
+    }
+    void Jump()
+    {
+        rb.velocity = new Vector2 (rb.velocity.x, jumpForce);
     }
 }
